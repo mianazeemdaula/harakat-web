0444987cc761b88668d72603658434d81f95439c
mianazeemdaula <mianazeemdaula@gmail.com>
2023-02-15 18:41:23 +0500

    rider

--- routes/api.php
+++ routes/api.php
@@ -4,21 +4,30 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\Api\AuthCustomerController;
+use App\Http\Controllers\Api\AuthRiderController;
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
 
 Route::get('users/{id}', function ($id) {
     $data = \App\Models\User::find($id)->products;
      return response()->json($data, 200, [],JSON_PRETTY_PRINT);
 });
 
+Route::prefix('customer')->group(function () {
+    Route::post('/login', [AuthCustomerController::class,'login']);
+    Route::post('/signup', [AuthCustomerController::class,'signup']);
+    Route::post('/social', [AuthCustomerController::class,'social']);
 
-Route::get('cutomer/login', [AuthCustomerController::class,'login']);
-Route::get('cutomer/signup', [AuthCustomerController::class,'signup']);
-Route::get('cutomer/social', [AuthCustomerController::class,'social']);
+    Route::middleware(['auth:sanctum'])->group(function () {
+        
+    });
+});
 
-Route::middleware(['auth:sanctum'])->group(function () {
-    
+Route::prefix('rider')->group(function () {
+    Route::post('/login', [AuthRiderController::class,'login']);
+    Route::post('/signup', [AuthRiderController::class,'signup']);
+    Route::post('/social', [AuthRiderController::class,'social']);
+
+    Route::middleware(['auth:sanctum'])->group(function () {
+        
+    });
 });
