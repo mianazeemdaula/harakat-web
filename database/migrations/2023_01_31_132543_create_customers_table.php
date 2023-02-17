9ecc5538e16670a199b8d53c92feba46ee17b99e
mianazeemdaula <mianazeemdaula@gmail.com>
2023-02-17 18:39:08 +0500

    user

--- database/migrations/2023_01_31_132543_create_customers_table.php
+++ database/migrations/2023_01_31_132543_create_customers_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
     {
         Schema::create('customers', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('user_id');
             $table->unsignedBigInteger('city_id');
             $table->date('dob')->nullable();
             $table->char('gender',10)->default('male');
