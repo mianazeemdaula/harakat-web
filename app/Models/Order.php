41a39897e7c29eea4f1e8439485c02d977fed652
mianazeemdaula <mianazeemdaula@gmail.com>
2023-03-14 14:49:19 +0500

    d

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -4,8 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use MatanYadaev\EloquentSpatial\Objects\Point;
+use MatanYadaev\EloquentSpatial\Traits\HasSpatial;
 
 class Order extends Model
 {
     use HasFactory;
+
+    protected $casts = [
+        'drop_location' => Point::class,
+    ];
+
+    public function newEloquentBuilder($query): SpatialBuilder
+    {
+        return new SpatialBuilder($query);
+    }
 }
