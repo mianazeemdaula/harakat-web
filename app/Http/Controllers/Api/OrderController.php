8d3cfff99f74f0efd36b5c6cbdbcbb52c76abd79
mianazeemdaula <mianazeemdaula@gmail.com>
2023-03-16 17:08:55 +0500

    sdf

--- app/Http/Controllers/Api/OrderController.php
+++ app/Http/Controllers/Api/OrderController.php
@@ -99,7 +99,8 @@ class OrderController extends Controller
      */
     public function show($id)
     {
-        //
+        $order= Order::with(['payment', 'details','user', 'shop'])->find($id);
+            return response()->json($order, 200);
     }
 
     /**
